2bfc4fdd833b3be577b73f0f6607843703c91001
2Fwebd <help@2f-design.fr>
2017-02-07 21:36:02 +0100

    - Deleted the welcome.blade as it won’t be needed in the next steps - Created the views architecture as well as the main HTML markup for the page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,13 @@
-Hello World
+@extends('layouts.frontend')
+
+@section('title', 'Welcome !')
+
+@section('content')
+
+    <div class="container-fluid">
+
+        <h1>Hello World!</h1>
+
+    </div>
+
+@endsection
---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+
+        <title>@yield('title') &mdash; 1000px</title>
+
+    </head>
+
+<body>
+
+    <div id="app-wrapper">
+
+        <section id="app-content">
+            @yield('content')
+        </section>
+
+    </div>
+
+</body>
+</html>
